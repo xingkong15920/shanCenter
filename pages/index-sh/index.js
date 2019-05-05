a6945b669f1feeabaef125397a8d1910f37261d8
longlei6666 <894640485@qq.com>
2019-05-05 10:36:14 +0800

    增加自注册页面，首页补充资料

--- pages/index-sh/index.js
+++ pages/index-sh/index.js
@@ -9,25 +9,25 @@ Page({
         userInfo: {},
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
-		server:config.server,
-		startT: Moment(new Date()).format('YYYY-MM-DD').split('-')[0] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[1] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[2],
-		endT: Moment(new Date()).format('YYYY-MM-DD').split('-')[0] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[1] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[2],
+        server: config.server,
+        startT: Moment(new Date()).format('YYYY-MM-DD').split('-')[0] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[1] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[2],
+        endT: Moment(new Date()).format('YYYY-MM-DD').split('-')[0] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[1] + '-' + Moment(new Date()).format('YYYY-MM-DD').split('-')[2],
         sList: [{
             name: "添加门店",
-			icon: "icon-mendian",
+            icon: "icon-mendian",
             color: "#82c529",
-			tap:"register"
+            tap: "register"
         }, {
             name: "门店管理",
             icon: "icon-dianpuguanli",
             color: "#ffba00",
-			tap:"shop",
-			num:0
+            tap: "shop",
+            num: 0
         }, {
             name: "店员管理",
             icon: "icon-renyuan",
             color: "#0087f2",
-			tap:'people'
+            tap: 'people'
         }],
         sList1: [{
             name: "设置管理",
@@ -40,21 +40,34 @@ Page({
         }, {
             name: "官方活动",
             icon: "icon-tuiguang",
-				color: "#ffba00"
+            color: "#ffba00"
         }],
-		imgUrls:['../img/1.png','../img/2.png','../img/3.png'],
-		indicatorDots: true,
-		autoplay: true,
-		interval:3000,
-		duration: 0,
-		moneyD:0,
-		shopD:0,
-		moneyA:0,
-		shopA:0,
+        // sList2: [{
+        //     name: "补充资料",
+        //     icon: "icon-shezhi",
+        //     color: "#0093e2",
+        //     tap: "updates"
+        // }],
+        settlement: [{
+            value: "D1",
+            tips: "*第二天到账(包含休息日,节假日)"
+        }, {
+            value: "D0",
+            tips: "*为全年当天到账(包含休息日,节假日)"
+        }],
+        imgUrls: ['../img/1.png', '../img/2.png', '../img/3.png'],
+        indicatorDots: true,
+        autoplay: true,
+        interval: 3000,
+        duration: 0,
+        moneyD: 0,
+        shopD: 0,
+        moneyA: 0,
+        shopA: 0,
+        // 结算弹窗
+        showModal: false,
+        settlementtype: '',
 
-
-
-	
     },
     //事件处理函数
     bindViewTap: function() {
@@ -62,99 +75,99 @@ Page({
             url: '../logs/logs'
         })
     },
-	changeAutoplay(e) {
-		this.setData({
-			autoplay: !this.data.autoplay
-		})
-	},
-	intervalChange(e) {
-		this.setData({
-			interval: e.detail.value
-		})
-	},
-	durationChange(e) {
-		this.setData({
-			duration: e.detail.value
-		})
-	},
-	getToday: function () {
-		var that = this
-		var sList = that.data.sList
-		wx.request({
+    changeAutoplay(e) {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+    intervalChange(e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+    durationChange(e) {
+        this.setData({
+            duration: e.detail.value
+        })
+    },
+    getToday: function() {
+        var that = this
+        var sList = that.data.sList
+        wx.request({
             url: this.data.server + 'merchantManage/getTransicationToday', //仅为示例，并非真实的接口地址
-			data: {
-				merchantNumber: that.data.saleNumber,
-				startTime: that.data.startT + ' ' + '00:00:00',
-				endTime: that.data.startT + ' ' + '23:59:59'
-			},
-			header: {
-				'content-type': 'application/json' // 默认值
-			},
-			success: function (res) {
-				console.log(res)
+            data: {
+                merchantNumber: that.data.saleNumber,
+                startTime: that.data.startT + ' ' + '00:00:00',
+                endTime: that.data.startT + ' ' + '23:59:59'
+            },
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success: function(res) {
+                console.log(res)
 
-				if (res.data.code != 1000) {
+                if (res.data.code != 1000) {
 
-				} else {
-					// console.log(res.data.data.newShopList[0])
+                } else {
+                    // console.log(res.data.data.newShopList[0])
                     console.log(res.data.data.shopCount)
-					for (var i = 0; i < sList.length;i++){
-						if (sList[i].tap == 'shop'){
+                    for (var i = 0; i < sList.length; i++) {
+                        if (sList[i].tap == 'shop') {
                             sList[i].num = res.data.data.shopCount
-						}
+                        }
                     }
-					that.setData({
+                    that.setData({
                         "moneyD": res.data.data.transicationMap.transactionAmount,
                         "shopD": res.data.data.transicationMap.transactionCount,
-						sList: sList
-					})
-					
-				}
-			}
-		})
-	},
+                        sList: sList
+                    })
+
+                }
+            }
+        })
+    },
     onLoad: function() {
-		var that = this
-		var saleInfo = wx.getStorageSync('shopInfo')
-		console.log(saleInfo)
-		this.setData({
-			institutionNumber: saleInfo.institutionNumber,
-			saleNumber: saleInfo.Number
-		})
-		that.getToday()
-		// wx.request({
-		// 	url: this.data.server + 'saleShareProfit/getSaleShop', //仅为示例，并非真实的接口地址
-		// 	data: {
-		// 		saleNumber: '180803111118022a',
-		// 		startTime: that.data.startT + ' ' + '00:00:00',
-		// 		endTime: that.data.endT + ' ' + '23:59:59'
-		// 	},
-		// 	header: {
-		// 		'content-type': 'application/json' // 默认值
-		// 	},
-		// 	success: function (res) {
-		// 		console.log(res.data.data.newShopList)
+        var that = this
+        var saleInfo = wx.getStorageSync('shopInfo')
+        console.log(saleInfo)
+        this.setData({
+            institutionNumber: saleInfo.institutionNumber,
+            saleNumber: saleInfo.Number
+        })
+        that.getToday()
+        // wx.request({
+        // 	url: this.data.server + 'saleShareProfit/getSaleShop', //仅为示例，并非真实的接口地址
+        // 	data: {
+        // 		saleNumber: '180803111118022a',
+        // 		startTime: that.data.startT + ' ' + '00:00:00',
+        // 		endTime: that.data.endT + ' ' + '23:59:59'
+        // 	},
+        // 	header: {
+        // 		'content-type': 'application/json' // 默认值
+        // 	},
+        // 	success: function (res) {
+        // 		console.log(res.data.data.newShopList)
 
-		// 		if (res.data.code != 1000) {
+        // 		if (res.data.code != 1000) {
 
-		// 		} else {
-		// 			console.log(res)
-		// 			var sList = that.data.sList
-		// 			for(var i = 0;i<sList.length;i++){
-		// 				if (sList[i].tap == 'shop'){
-		// 					sList[i].num = '(' +  res.data.data.merchantCount + ')'
-		// 				}
-		// 			}
-		// 			that.setData({
-		// 				moneyD: res.data.data.newShopList[0].settlementMoney,
-		// 				shopD: res.data.data.newShopList[0].shopCount,
-		// 				moneyA: res.data.data.todayMoney,
-		// 				shopA: res.data.data.todayCount,
-		// 				sList: sList
-		// 			})
-		// 		}
-		// 	}
-		// })
+        // 		} else {
+        // 			console.log(res)
+        // 			var sList = that.data.sList
+        // 			for(var i = 0;i<sList.length;i++){
+        // 				if (sList[i].tap == 'shop'){
+        // 					sList[i].num = '(' +  res.data.data.merchantCount + ')'
+        // 				}
+        // 			}
+        // 			that.setData({
+        // 				moneyD: res.data.data.newShopList[0].settlementMoney,
+        // 				shopD: res.data.data.newShopList[0].shopCount,
+        // 				moneyA: res.data.data.todayMoney,
+        // 				shopA: res.data.data.todayCount,
+        // 				sList: sList
+        // 			})
+        // 		}
+        // 	}
+        // })
         if (app.globalData.userInfo) {
             this.setData({
                 userInfo: app.globalData.userInfo,
@@ -182,9 +195,9 @@ Page({
             })
         }
     },
-	onShow:function(){
-		this.getToday()
-	},
+    onShow: function() {
+        this.getToday()
+    },
     getUserInfo: function(e) {
         console.log(e)
         app.globalData.userInfo = e.detail.userInfo
@@ -195,27 +208,75 @@ Page({
     },
     record: function() {
         wx.navigateTo({
-			url: '../shop-sh/shop-payment/index',
+            url: '../shop-sh/shop-payment/index',
         })
     },
-	register: function () {
-		wx.navigateTo({
-			url: '../shop-sh/shop-check/index',
-		})
-	},
+    register: function() {
+        wx.navigateTo({
+            url: '../shop-sh/shop-check/index',
+        })
+    },
     salekit: function() {
         wx.navigateTo({
-			url: '../salekit-sh/index',
+            url: '../salekit-sh/index',
         })
     },
     shop: function() {
         wx.navigateTo({
-			url: '../shop-sh/shop-list/index',
+            url: '../shop-sh/shop-list/index',
         })
     },
-	people:function(){
-		wx.navigateTo({
-			url: '../shop-sh/assistant/index',
-		})
-	}
+    people: function() {
+        wx.navigateTo({
+            url: '../shop-sh/assistant/index',
+        })
+    },
+    chooseType: function(e) {
+        this.setData({
+            settlementtype: e.detail.value
+        })
+    },
+    /**
+     * 弹窗
+     */
+    updates: function() {
+        this.setData({
+            showModal: true
+        })
+    },
+    /**
+     * 弹出框蒙层截断touchmove事件
+     */
+    preventTouchMove: function() {},
+    /**
+     * 隐藏模态对话框
+     */
+    hideModal: function() {
+        this.setData({
+            showModal: false,
+            settlementtype: ''
+        });
+    },
+    /**
+     * 对话框取消按钮点击事件
+     */
+    onCancel: function() {
+        this.hideModal();
+    },
+    /**
+     * 对话框确认按钮点击事件
+     */
+    onConfirm: function() {
+        if (this.data.settlementtype == '') {
+            wx.showToast({
+                title: '请选择结算方式！',
+                icon: 'none',
+            })
+        } else {
+            console.log(this.data.settlementtype)
+            wx.navigateTo({
+                url: '../merchants/register/index',
+            })
+        }
+    }
 })
